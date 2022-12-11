3dbe81d9de59e9b55996d6eee41928413f64e8c8
shrivastav1811 <sonushrivastav1811@gmail.com>
2022-12-12 00:04:41 +0530

    taginput add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { React, useEffect, useRef, useState } from "react";
+import tagImg from "./tag.png";
 
 function App() {
+  const [tags, setTags] = useState([]);
+  const [inputValue, setInputValue] = useState();
+  let maxtags = 10
+
+  const addtags = (e) => {
+    if (e.key == "Enter") {
+      if (inputValue.length > 1 && !tags.includes(inputValue)) {
+        if (tags.length < 10) {
+          inputValue.split(",").forEach((tg) => {
+            console.log(tg);
+            setTags([...tags, tg]);
+          });
+        }
+      }
+      e.target.value = "";
+    }
+  };
+
+  const removetags = (index) => {
+    setTags([...tags.filter((tag) => tags.indexOf(tag) !== index)]);
+  };
+
+  const handleRemoveAll = () => {
+    setTags([])
+  }
+
+  const handlechange = (e) => {
+    setInputValue(e.target.value.replace(/\+/g, " "));
+  };
+  console.log(tags);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrapper">
+      <div className="title">
+        <img src={tagImg} alt="" />
+        <h2>Tags</h2>
+      </div>
+      <div className="content">
+        <p>Press enter or add a comma after each tag</p>
+
+        <ul>
+          {tags.map((tag, index) => (
+            <li key={index}>
+              {tag}{" "}
+              <i
+                className="uit uit-multiply"
+                onClick={() => removetags(index)}
+              ></i>
+            </li>
+          ))}
+          <input
+            type="text"
+            placeholder="Press enter to add tags"
+            onKeyUp={(e) => addtags(e)}
+            onChange={handlechange}
+          />
+        </ul>
+      </div>
+      <div className="details">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>{ maxtags-tags.length}</span> tags are remaining
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={handleRemoveAll}>Remove All</button>
+      </div>
     </div>
   );
 }
